55bf10c34938df968111ea650860cd374c32fc63
neagtivefriction <chris.chrispete@gmail.com>
2019-03-12 09:39:58 -0500

    Made gifs pause. Added minor cs.

--- logic.js
+++ logic.js
@@ -12,19 +12,44 @@ $(document).ready(function() {
     "The Princess Bride"
   ];
   var key = "Jw4O2FCSkhYkZttjtxqhnDEG3vSMwSIU";
-  var key2 = "Jw4O2FCSkhYkZttjtxqhnDEG3vSMwSIU";
-  var buttonSubject = "beer";
 
-  for (var i = 0; i < topics.length; i++) {
-    var newButton = $("<button class = 'topicButton'>");
-    newButton.text(topics[i]);
-    $("#categories").append(newButton);
+  var buttonSubject;
+  buttonDrawer();
+
+  function buttonDrawer() {
+    $("#categories").empty();
+    for (var i = 0; i < topics.length; i++) {
+      var newButton = $("<button class = 'topicButton'>");
+      newButton.text(topics[i]);
+      $("#categories").append(newButton);
+    }
   }
 
   $(document).on("click", ".topicButton", function() {
     console.log("Click");
     buttonSubject = $(this).text();
     ajaxCall();
+  });
+
+  $(document).on("click", ".gif", function() {
+    if ($(this).attr("data-state") === "paused") {
+      $(this).attr("data-state", "animated");
+      $(this).attr("src", $(this).attr("data-moving"));
+    } else {
+      $(this).attr("data-state", "paused");
+      $(this).attr("src", $(this).attr("data-still"));
+    }
+  });
+
+  $("#searchButton").on("click", function() {
+    event.preventDefault();
+    topics.push($("#gifToSearch").val());
+
+    buttonSubject = $("#gifToSearch").val();
+    ajaxCall();
+    $("#gifToSearch").val("");
+
+    buttonDrawer();
   });
 
   function ajaxCall() {
@@ -35,24 +60,28 @@ $(document).ready(function() {
       "&q=" +
       buttonSubject +
       "&offset=0&rating=PG&lang=en&limit=10";
-    console.log("called ajax");
 
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      console.log(response.data);
-      //   var newImg = $("<img>");
-      //   newImg.attr("src", response.data[0].images.fixed_height.url);
-      //   $("#gifDisplayArea").append(newImg);
       for (var i = 0; i < response.data.length; i++) {
-        var newImg = $("<img>");
-        newImg.attr("src", response.data[i].images.fixed_height.url);
-        $("#gifDisplayArea").append(newImg);
-        // console.log(newImg);
+        var container = $("<div class='container'>");
+        var newImg = $("<img class = 'gif'>");
+        newImg.attr("src", response.data[i].images.fixed_height_still.url);
+        newImg.attr(
+          "data-still",
+          response.data[i].images.fixed_height_still.url
+        );
+        newImg.attr("data-moving", response.data[i].images.fixed_height.url);
+        newImg.attr("data-state", "paused");
+        var descriptorDiv = $("<p>");
+        descriptorDiv.attr("class", "descriptor");
+        descriptorDiv.text("rating: " + response.data[i].rating);
+        $(container).append(newImg);
+        $(container).append(descriptorDiv);
+        $("#gifDisplayArea").append(container);
       }
     });
   }
-
-  ajaxCall();
 });
